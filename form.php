1ecf5507c28df827f55973e5d814138cae97f4f7
kaz1027 <kazuki102777@gmail.com>
2021-09-14 17:45:51 +0900

    9/14 1回目

--- form.php
+++ form.php
@@ -1,20 +1,47 @@
 <?php
     session_start();
     $mode = 'input';
-    if ( isset($_POST['back']) && $_POST['back']) {
-        // 何もしない
-    } else if ( isset($_POST['confirm']) && $_POST['confirm']) {
-        $_SESSION['fullname'] = $_POST['fullname'];
-        $_SESSION['email']    = $_POST['email'];
-        $_SESSION['message']  = $_POST['message'];
-        $mode = 'confirm';
-    } else if ( isset($_POST['send']) && $_POST['send']) {
+    $errmessage = array();
+    if( isset($_POST['back']) && $_POST['back'] ){
+      // 何もしない
+    } else if( isset($_POST['confirm']) && $_POST['confirm'] ){
+  	  // 確認画面
+        if( !$_POST['fullname'] ) {
+            $errmessage[] = "名前を入力してください";
+        } else if( mb_strlen($_POST['fullname']) > 100 ){
+            $errmessage[] = "名前は100文字以内にしてください";
+        }
+        $_SESSION['fullname']	= htmlspecialchars($_POST['fullname'], ENT_QUOTES);
+
+        if( !$_POST['email'] ) {
+            $errmessage[] = "Eメールを入力してください";
+        } else if( mb_strlen($_POST['email']) > 200 ){
+            $errmessage[] = "Eメールは200文字以内にしてください";
+        } else if( !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+            $errmessage[] = "メールアドレスが不正です";
+        }
+
+        $_SESSION['email']	= htmlspecialchars($_POST['email'], ENT_QUOTES);
+        if( !$_POST['message'] ){
+            $errmessage[] = "お問い合わせ内容を入力してください";
+        } else if( mb_strlen($_POST['message']) > 500 ){
+            $errmessage[] = "お問い合わせ内容は500文字以内にしてください";
+        }
+        $_SESSION['message'] = htmlspecialchars($_POST['message'], ENT_QUOTES);
+
+        if( $errmessage ){
+            $mode = 'input';
+        } else {
+            $mode = 'confirm';
+        }
+    } else if( isset($_POST['send']) && $_POST['send'] ){
+      // 送信ボタンを押したとき
         $message  = "お問い合わせを受け付けました \r\n"
                 . "名前: " . $_SESSION['fullname'] . "\r\n"
                 . "email: " . $_SESSION['email'] . "\r\n"
                 . "お問い合わせ内容:\r\n"
                 . preg_replace("/\r\n|\r|\n/", "\r\n", $_SESSION['message']);
-        mail($_SESSION['email'],'お問い合わせありがとうございます',$message);
+        mail($_SESSION['email'],'お問い合わせありがとうございます。',$message);
         mail('nspdesign2012@gmail.com','お問い合わせを受け付けました',$message);
         $_SESSION = array();
         $mode = 'send';
@@ -36,23 +63,25 @@
 <body>
     <?php if ( $mode == 'input' ){ ?>
         <!-- 入力画面 -->
-        <div class="form">
-            <form action="./form.php" method="POST">
 
+        <?php
+            if( $errmessage ){
+                echo '<div style = "color:red" >';
+                echo implode('<br>', $_errmessage);
+                echo '</div>';
+            }
+        ?>
+        <form action="./form.php" method="POST">
             <p class="form_contents">名前</p>
             <input type="text" name="fullname" value="<?php echo $_SESSION['fullname'] ?>"><br>
-
             <p class="form_contents">E-mail</p>
             <input type="email" name="email" value="<?php echo $_SESSION['email'] ?>"><br>
-
             <p class="form_contents">お問い合わせ内容</p>
             <textarea name="message" id="" cols="30" rows="10"><?php echo $_SESSION['message'] ?></textarea>
-
             <div class="btn">
                 <input type="submit" name="confirm" value="確認">
             </div>
-            </form>
-        </div>
+        </form>
     <?php } else if ( $mode == 'confirm' ){ ?>
         <!-- 確認画面 -->
         <div class="form">
@@ -63,7 +92,7 @@
             <p class="form_contents">Eメール</p>
             <?php echo $_SESSION['email'] ?><br>
 
-            <p class="form_contents">お問い合わせ内容</p><br>
+            <p class="form_contents">お問い合わせ内容あ</p><br>
             <?php echo nl2br($_SESSION['message']) ?><br>
 
             <div class="btn">
@@ -74,7 +103,7 @@
         </div>
     <?php } else { ?>
         <!-- 完了画面 -->
-        送信完了しました。お問い合わせありがとうございました。<br>
+        送信しました。お問い合わせありがとうございました。<br>
         <div class="btn">
             <a href="./index.php">トップページへ</a>
         </div>
